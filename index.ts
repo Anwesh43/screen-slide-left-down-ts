ae87a011f640760dfdfffa7d732ad18f2133b287
Anwesh43 <anweshthecool0@gmail.com>
2021-01-27 05:25:10 +0530

    adding fixes

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const scGap : number = 0.02 
-const parts : number = 5 
+const parts : number = 2
 const strokeFactor : number = 90 
 const sizeFactor : number = 2.9 
 const colors : Array<string> = [
@@ -86,7 +86,7 @@ class State {
     prevScale : number = 0 
 
     update(cb : Function) {
-        this.scale += scGap 
+        this.scale += scGap * this.dir
         if (Math.abs(this.scale - this.prevScale) > 1) {
             this.scale = this.prevScale + this.dir 
             this.dir = 0 
